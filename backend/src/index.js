6ae05282ac9f812b0ac292a9c3a67b47af0b1dbb
idriss-elouiri <idriss1elouiri2@gmail.com>
2025-05-08 12:08:09 +0100

    save changes

--- backend/src/index.js
+++ backend/src/index.js
@@ -16,7 +16,14 @@ connectDb();
 // Middleware
 app.use(express.json());
 app.use(cookieParser());
-
+app.use(
+    cors({
+        origin: process.env.FRONTEND_URL,
+        credentials: true,
+        methods: ["GET", "POST", "PUT", "DELETE"],
+        allowedHeaders: ["Content-Type", "Authorization"],
+    })
+);
 app.get('/', (req, res) => {
     res.send({
         activeStatus: true,
